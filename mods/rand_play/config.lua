5916f0ccbc6d6a29b02f80ab66924c9705eff8b6
azekillDIABLO <Rodrigue.Lemaire@gmail.com>
2016-04-17 15:01:43 +0200

    Added files via upload

---
+++ mods/rand_play/config.lua
@@ -0,0 +1,6 @@
+-- Play time of the longest song in the list in seconds( suggest adding maybe 10 seconds to be safe )
+longest = 395
+-- amount of time to wait after server starts up to begin playing in seconds
+delay = 20
+-- volume or gain or level to play the music ( default was set for sample music )
+playvol = .5
